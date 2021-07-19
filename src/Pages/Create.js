29de4ede8dd18e19d46a2b0a02e107cb2ebfef6f
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-07-19 14:38:29 +0530

    com

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,17 +1,5 @@
-/**
- * @description      :
- * @author           : pasin
- * @group            :
- * @created          : 19/07/2021 - 02:43:43
- *
- * MODIFICATION LOG
- * - Version         : 1.0.0
- * - Date            : 19/07/2021
- * - Author          : pasin
- * - Modification    :
- **/
 import React from "react";
 
 export default function Create() {
-    return <div > Create page < /div>;
+    return <div > Create page </div>;
 }
