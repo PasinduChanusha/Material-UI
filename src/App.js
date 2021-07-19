5ddb4cd3ecb6f7ef9cbf49f7d81d6aac0d1425f2
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-07-19 18:23:00 +0530

    advertise

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { BrowwerRouter as Router, Switch, Route } from "react-router-dom";
-import Notes from "./pages/Notes";
-import Create from "./Pages/Create";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Notes from './pages/Notes'
+import Create from './pages/Create'
 
 function App() {
-    return ( 
-        <Router >
-            <switch >
-                <Route exact path = "/" >
-                    <Notes/>
-                </Route>
-                <Route path = "./create" >
-                    <Create />
-                </Route> 
-            </switch> 
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function Create() {
-    return (
-        <div> 
-            Create page 
-        </div>
+  const classes = useStyles();
+  return (
 
-    )
-    
+    <div>
+      <Typography 
+      varient='h1'
+      color='primary'
+      align='center'
+      gutterBottom
+      >
+      <b>WSO2</b>
+      </Typography>
+
+      <Button
+        variant="outlined"
+        size="large" 
+        color="secondary" 
+        disableElevation
+        className={classes.margin}
+        endIcon={<Icon>send</Icon>}
+      >
+        Make New Job Advertisement
+      </Button>
+    </div>
+  )
 }
